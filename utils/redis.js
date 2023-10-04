53ed980447def58cceb58cb56265657b7e1ffb1c
sezfabian <cheruiyotfabian@gmail.com>
2023-10-04 23:16:10 +0300

    comments

--- utils/redis.js
+++ utils/redis.js
@@ -1,4 +1,18 @@
 #!/usr/bin/node
+/*
+contains the class RedisClient
+constructor that creates a client to Redis:
+any error of the redis client must be displayed in the console
+a function isAlive that returns true when the connection to Redis is a success
+otherwise, false
+an asynchronous function get that takes a string key as argument
+and returns the Redis value stored for this key
+an asynchronous function set that takes a string key, a value
+and a duration in second as arguments to store it in Redis
+(with an expiration set by the duration argument)
+an asynchronous function del that takes a string key as argument
+and remove the value in Redis for this key
+*/
 
 const redis = require('redis');
 
